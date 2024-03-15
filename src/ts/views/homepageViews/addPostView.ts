39d9df124c0e51e8c7a2668bf95d4ae5ffb618f3
dzoniradilica <dzonidzonatan2@gmail.com>
2024-03-15 15:33:05 +0100

    Refectoring render post

--- src/ts/views/homepageViews/addPostView.ts
+++ src/ts/views/homepageViews/addPostView.ts
@@ -20,112 +20,62 @@ class AddPostView {
   }
 
   createPost(postData: ConfigPost, userData: ConfigUser) {
-    const html = `
-    <div class="post">
-        <div class="person-info">
-            <img src="images/person.png" alt="person" />
-            <div>
-                <h3 class="author">${userData.username}</h3>
-                <p class="date">2 days ago</p>
-            </div>
-        </div>
+    this.renderPost('post', postData, userData);
 
-        <div class="post-content">
-            <p>${postData.content}</p>
-        </div>
-
-        <div class="like-comments-wrapper">
-            <span id="likes">${postData.likes}</span>
-            <img src="images/heart.png" alt="like" class="heart" />
-            <img src="images/comment (1).png" alt="comment" />
-
-            <button id="deletePost">Delete</button>
-        </div>
-    </div>
-    `;
-
-    this.parentElement.insertAdjacentHTML('afterbegin', html);
-    (document.querySelector('#addPostContent')! as HTMLInputElement).value = '';
+    const postInput = document.querySelector(
+      '#addPostContent'
+    )! as HTMLInputElement;
+    postInput.value = '';
   }
 
   displayAllPosts(postData: ConfigPost[], singleUser: ConfigUser) {
     this.parentElement.innerHTML = '';
-    postData.forEach(singlePost => {
-      const html = `
-        <div class="post">
-            <div class="person-info">
-                <img src="images/person.png" alt="person" />
-                <div>
-                
-                    <h3 class="author">${singlePost.author}</h3>
-                    <p class="date">2 days ago</p>
-                </div>
-            </div>
-    
-            <div class="post-content">
-                <p>${singlePost.content}</p>
-            </div>
-    
-            <div class="like-comments-wrapper">
-                <span id="likes">${singlePost.likes}</span>
-                <img src="images/heart.png" alt="like" class="heart" />
-                <img src="images/comment (1).png" alt="comment" />
-    
-                ${
-                  singleUser.username === singlePost.author
-                    ? '<button id="deletePost">Delete</button>'
-                    : ''
-                }
-            </div>
-        </div>
-        `;
 
-      this.parentElement.insertAdjacentHTML('afterbegin', html);
-      //   this.parentElement.innerHTML = '';
+    postData.forEach(singlePost => {
+      this.renderPost('posts', singlePost, singleUser);
     });
   }
 
-  //   private renderPost(
-  //     type: 'post' | 'posts',
-  //     singlePost: ConfigPost,
-  //     userData: ConfigUser
-  //   ) {
-  //     const html = `
-  //         <div class="post">
-  //             <div class="person-info">
-  //                 <img src="images/person.png" alt="person" />
-  //                 <div>
-  //                     <h3 class="author">${
-  //                       type === 'post'
-  //                         ? `<h3 class="author">${userData.username}</h3>`
-  //                         : +singlePost.userId === +userData.id
-  //                         ? userData.username
-  //                         : ''
-  //                     }</h3>
-  //                     <p class="date">2 days ago</p>
-  //                 </div>
-  //             </div>
+  private renderPost(
+    type: 'post' | 'posts',
+    singlePost: ConfigPost,
+    userData: ConfigUser
+  ) {
+    const html = `
+          <div class="post">
+              <div class="person-info">
+                  <img src="images/person.png" alt="person" />
+                  <div>
+                      <h3 class="author">${
+                        type === 'post'
+                          ? `<h3 class="author">${userData.username}</h3>`
+                          : singlePost.author
+                      }</h3>
+                      <p class="date">2 days ago</p>
+                  </div>
+              </div>
 
-  //             <div class="post-content">
-  //                 <p>${singlePost.content}</p>
-  //             </div>
+              <div class="post-content">
+                  <p>${singlePost.content}</p>
+              </div>
 
-  //             <div class="like-comments-wrapper">
-  //                 <span id="likes">${singlePost.likes}</span>
-  //                 <img src="images/heart.png" alt="like" class="heart" />
-  //                 <img src="images/comment (1).png" alt="comment" />
+              <div class="like-comments-wrapper">
+                  <span id="likes">${singlePost.likes}</span>
+                  <img src="images/heart.png" alt="like" class="heart" />
+                  <img src="images/comment (1).png" alt="comment" />
 
-  //                 ${
-  //                     type === 'post' ? `<button id="deletePost">Delete</button>` :
-  //                   +singlePost.userId === +userData.id
-  //                     ? '<button id="deletePost">Delete</button>'
-  //                     : ''
-  //                 }
-  //             </div>
-  //         </div>
-  //         `;
-  //         this.parentElement.insertAdjacentHTML('afterbegin', html)
-  //   }
+                  ${
+                    type === 'post'
+                      ? `<button id="deletePost">Delete</button>`
+                      : singlePost.author === userData.username
+                      ? '<button id="deletePost">Delete</button>'
+                      : ''
+                  }
+              </div>
+          </div>
+          `;
+    this.parentElement.insertAdjacentHTML('afterbegin', html);
+  }
 }
 
 export const addPostView = new AddPostView();
