3e8331de60553c7681b157a8c1d6cc87b26f6338
dzoniradilica <dzonidzonatan2@gmail.com>
2024-03-17 16:19:50 +0100

    Finished display all users

--- src/ts/views/homepageViews/displayAllUsersView.ts
+++ src/ts/views/homepageViews/displayAllUsersView.ts
@@ -1,9 +1,22 @@
 import { ConfigUser } from '../../configs/user-config';
 
 class DisplayAllUsersView {
+  parentElement = document.querySelector('.users-wrapper')! as HTMLDivElement;
+
   displayAllUsers(allUsers: ConfigUser[]) {
     allUsers.forEach(singleUser => {
-      console.log(singleUser);
+      const html = `
+        <div class="users-inner">
+            <img src="images/person.png" alt="person" />
+            <h4 class="user-name">${singleUser.username}</h4>
+
+            <div class="btns-div">
+                <button id="sendMessage">Message</button>
+            </div>
+        </div>
+        `;
+
+      this.parentElement.insertAdjacentHTML('afterbegin', html);
     });
   }
 }
